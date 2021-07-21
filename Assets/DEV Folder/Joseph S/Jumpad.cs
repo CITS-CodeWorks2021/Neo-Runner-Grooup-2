d6ecabaa0bf08418096a15f3b80d3951a6d8051b
JosephSantos1 <86676990+JosephSantos1@users.noreply.github.com>
2021-07-21 10:24:06 -0400

    The jumpad
    Created a jumpad

---
+++ Assets/DEV Folder/Joseph S/Jumpad.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jumpad : MonoBehaviour
+{
+    public float jumpAmount;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+     Rigidbody2D rigidbody2D = collision.gameObject.GetComponent<Rigidbody2D>();
+        if (rigidbody2D)
+        {
+            rigidbody2D.AddForce(Vector2.up * jumpAmount, ForceMode2D.Impulse);
+        }
+    }
+}
